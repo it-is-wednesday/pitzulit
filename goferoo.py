e7bb8da599f776d6d6eede3cb2529bee7c330a20
tag-them <qoev@protonmail.com>
2017-11-06 01:24:28 +0200

    Added documentation

--- goferoo.py
+++ goferoo.py
@@ -9,6 +9,12 @@ FilePath = str
 
 
 def download_album(url: str) -> (FilePath, FilePath):
+        """
+        :param url: url of a full album video on youtube
+        :return: a tuple containing:
+                        1. path denoting a text file containing the video's description
+                        2. path denoting a WAV file containing the whole video's audio
+        """
         download_path = "downloaded"
 
         description_file = download_path + ".description"
@@ -21,6 +27,10 @@ def download_album(url: str) -> (FilePath, FilePath):
 
 
 def minutes_to_seconds(time) -> int:
+        """
+        :param time: seconds/minutes (can be either a string in MM:SS format or an integer)
+        :return: if time was an integer it returns itself; otherwise it's converted to seconds
+        """
         ptime = str(time)
         if ':' in ptime:
                 s = ptime.split(':')
@@ -31,6 +41,10 @@ def minutes_to_seconds(time) -> int:
 
 
 def album_length(file_path: FilePath) -> int:
+        """
+        :param file_path: path denoting a full album's audio file
+        :return: the album's length in seconds
+        """
         os.system("ffprobe -i {file_path} -show_entries format=duration -v quiet -of csv='p=0' > length"
                   .format(file_path=file_path))
         result = open("length").read()
@@ -38,7 +52,14 @@ def album_length(file_path: FilePath) -> int:
         return int(float(result))
 
 
-def extract_song(file_path: str, song_beginning: str, song_end: str, number: int) -> FilePath:
+def extract_track(file_path: str, song_beginning: str, song_end: str, number: int) -> FilePath:
+        """
+        :param file_path: path denoting a full album's audio file
+        :param song_beginning: beginning of a song we'd like to extract (either in minutes or seconds)
+        :param song_end: end of a song we'd like to extract (either in minutes or seconds)
+        :param number: track number
+        :return: path denoting an audio file containing the extracted track
+        """
         os.system("avconv -i '{input_file}' -ss {beginning} -t {end} '{output_file}' -y"
                   .format(beginning=song_beginning,
                           end=int(minutes_to_seconds(song_end)) - int(minutes_to_seconds(song_beginning)),
@@ -48,8 +69,15 @@ def extract_song(file_path: str, song_beginning: str, song_end: str, number: int
 
 
 def chop(timestamps: List[str], file_path: str) -> List[FilePath]:
+        """
+        :param timestamps: a list of timestamps for each track in the album, for example: ["0:00", "4:30", "8:12"]
+        :param file_path: path denoting an audio file containing the album's audio
+        :return: Extracts each track in the album into a separate file based on timestamps and returns a list of paths,
+                 each path referring to a different track file
+        """
+
         def e(start, end, number: int) -> FilePath:
-                return extract_song(file_path, start, end, number)
+                return extract_track(file_path, start, end, number)
 
         ts = timestamps
         songs_num = len(ts) + 1
@@ -76,6 +104,7 @@ if __name__ == '__main__':
         album_path, description_path = download_album(video_url)
 
         video_description = open(description_path).read()
+        # find timestamps in the description
         timestamps = re.findall(r'\d\d:\d\d', video_description)
 
         print(chop(timestamps, album_path))
