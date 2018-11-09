d2285b30d4e9011a06cd85795602063f7b4db71a
Maor Nadosh <qoev@protonmail.com>
2018-11-09 09:47:56 +0200

    :shower:

--- src/pitzulit_types.ml
+++ src/pitzulit_types.ml
@@ -1,13 +1,12 @@
 type track_info =
-    { song_title : string
-    ; time_seconds : int
-    ; length_seconds : int
-    }
-
+  { song_title : string
+  ; time_seconds : int
+  ; length_seconds : int
+  }
 
 type video_info =
-    { video_title : string
-    ; description : string
-    ; duration_seconds : int
-    ; thumbnail_url : string
-    }
+  { video_title : string
+  ; description : string
+  ; duration_seconds : int
+  ; thumbnail_url : string
+  }
--- src/utils.ml
+++ src/utils.ml
@@ -1,6 +1,6 @@
 let lwt_shell cmd =
-    let open Lwt.Infix in
-    let run_and_wait cmd =
-        cmd |> Lwt_process.shell |> Lwt_process.exec >|= fun _ -> ()
-    in
-    Printf.ksprintf run_and_wait cmd
+  let open Lwt.Infix in
+  let run_and_wait cmd =
+    cmd |> Lwt_process.shell |> Lwt_process.exec >|= fun _ -> ()
+  in
+  Printf.ksprintf run_and_wait cmd
